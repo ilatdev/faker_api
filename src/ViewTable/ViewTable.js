d3b845d1cf12f6434dacbe89a330b7f886576854
Ian Latour <latourian@gmail.com>
2021-07-22 17:41:07 -0300

    extract paginationActions from ViewTable file

--- src/ViewTable/ViewTable.js
+++ src/ViewTable/ViewTable.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { makeStyles, useTheme } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import {
   Paper,
   Table,
@@ -9,30 +9,18 @@ import {
   TableRow,
   TableCell,
   TableFooter,
-  TablePagination
+  TablePagination,
+  Box
 } from '@material-ui/core'
 import CircularProgress from '@material-ui/core/CircularProgress'
+
 import ItemTable from './ItemTable/ItemTable'
-import IconButton from '@material-ui/core/IconButton'
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft'
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight'
-import LastPageIcon from '@material-ui/icons/LastPage'
-import FirstPageIcon from '@material-ui/icons/FirstPage'
+import PaginationActions from './PaginationActions'
 
-const useStyles1 = makeStyles((theme) => ({
-  root: {
-    flexShrink: 0,
-    marginLeft: theme.spacing(2.5)
-  }
-}))
-
-const useStyles2 = makeStyles((theme) => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: '2rem',
-    padding: '1rem 2rem'
+    justifyContent: 'center'
   },
   refRow: {
     textTransform: 'capitalize',
@@ -41,81 +29,22 @@ const useStyles2 = makeStyles((theme) => ({
   },
   table: {
     minWidth: 500,
-    backgroundColor: '#D8ECFD'
+    backgroundColor: '#edf6f9'
   }
 }))
 
-function TablePaginationActions(props) {
-  const classes = useStyles1()
-  const theme = useTheme()
-  const { count, page, rowsPerPage, onPageChange } = props
-
-  const handleFirstPageButtonClick = (event) => {
-    onPageChange(event, 0)
-  }
-
-  const handleBackButtonClick = (event) => {
-    onPageChange(event, page - 1)
-  }
-
-  const handleNextButtonClick = (event) => {
-    onPageChange(event, page + 1)
-  }
-
-  const handleLastPageButtonClick = (event) => {
-    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1))
-  }
-
-  return (
-    <div className={classes.root}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page">
-        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page">
-        {theme.direction === 'rtl' ? (
-          <KeyboardArrowRight />
-        ) : (
-          <KeyboardArrowLeft />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page">
-        {theme.direction === 'rtl' ? (
-          <KeyboardArrowLeft />
-        ) : (
-          <KeyboardArrowRight />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page">
-        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </div>
-  )
-}
-
 export default function ViewTable({ data, loading }) {
-  const cls = useStyles2()
+  const cls = useStyles()
   const [page, setPage] = useState(0)
   const [rowsPerPage, setRowsPerPage] = useState(5)
 
   if (loading) {
     return (
-      <div className={cls.root}>
-        <div>
+      <Box className={cls.root}>
+        <Box mt={10}>
           <CircularProgress size={100} disableShrink />
-        </div>
-      </div>
+        </Box>
+      </Box>
     )
   }
 
@@ -138,52 +67,57 @@ export default function ViewTable({ data, loading }) {
   }
 
   return (
-    <div className={cls.root}>
-      <TableContainer component={Paper}>
-        <Table className={cls.table} aria-label="custom pagination table">
-          <TableHead>
-            <TableRow>{tableHeader}</TableRow>
-          </TableHead>
-          <TableBody>
-            {(rowsPerPage > 0
-              ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              : data
-            ).map((el, index) => (
-              <TableRow key={`table-${index}`}>
-                {Object.values(el).map((value, cell) => (
-                  <TableCell align="justify" key={`row-${cell}`}>
-                    <ItemTable content={value} />
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
+    <Box className={cls.root}>
+      <Box my={5} px={5}>
+        <TableContainer component={Paper}>
+          <Table className={cls.table} aria-label="custom pagination table">
+            <TableHead>
+              <TableRow>{tableHeader}</TableRow>
+            </TableHead>
+            <TableBody>
+              {(rowsPerPage > 0
+                ? data.slice(
+                    page * rowsPerPage,
+                    page * rowsPerPage + rowsPerPage
+                  )
+                : data
+              ).map((el, index) => (
+                <TableRow hover key={`table-${index}`}>
+                  {Object.values(el).map((value, cell) => (
+                    <TableCell align="justify" key={`row-${cell}`}>
+                      <ItemTable content={value} />
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
 
-            {emptyRows > 0 && (
-              <TableRow style={{ height: 53 * emptyRows }}>
-                <TableCell colSpan={6} />
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                  colSpan={3}
+                  count={data.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  SelectProps={{
+                    inputProps: { 'aria-label': 'rows per page' },
+                    native: true
+                  }}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  ActionsComponent={PaginationActions}
+                />
               </TableRow>
-            )}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                colSpan={3}
-                count={data.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                SelectProps={{
-                  inputProps: { 'aria-label': 'rows per page' },
-                  native: true
-                }}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-                ActionsComponent={TablePaginationActions}
-              />
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </TableContainer>
-    </div>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Box>
   )
 }
